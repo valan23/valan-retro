3135bc52ad7f6ff7c8caf057bbf260b07fb35b5f
valan23 <carlosgmoya@gmail.com>
2026-02-03 08:48:50 +0100

    Update games.js

--- js/games.js
+++ js/games.js
@@ -54,7 +54,7 @@ function createCardHTML(j) {
     
             <div style="position: absolute; top: 0; left: 0; width: 100%; height: 45px; z-index: 10; display: flex; align-items: stretch;">
         
-                <div class="icon-gradient-area" style="flex: 0 0 calc(60% - 6px); border-top-left-radius: 10px;">
+                <div class="icon-gradient-area" style="flex: 0 0 calc(60% - 6px); border-top-left-radius: 11px;">
                     <div class="platform-icon-card" style="margin: 0; filter: drop-shadow(1px 1px 2px rgba(0,0,0,0.6));">
                         ${getPlatformIcon(plat)}
                     </div>
@@ -98,7 +98,7 @@ function createCardHTML(j) {
 
             <div style="margin-top: 10px; height: 55px; border-top: 1px solid rgba(255,255,255,0.05); display: flex; align-items: stretch; overflow: hidden;">
         
-                    <div style="flex: 1; background: ${bgFormato}; color: ${colorTextoFormato}; border-right: 1px solid rgba(255,255,255,0.05); margin-left: 6px; display: flex; flex-direction: column; align-items: center; justify-content: center; border-bottom-left-radius: 10px;">
+                    <div style="flex: 1; background: ${bgFormato}; color: ${colorTextoFormato}; border-right: 1px solid rgba(255,255,255,0.05); margin-left: 6px; display: flex; flex-direction: column; align-items: center; justify-content: center; border-bottom-left-radius: 11px;">
                         <i class="fa-solid ${esDigital ? 'fa-cloud-download' : 'fa-floppy-disk'}" style="font-size: 1em; margin-bottom: 2px;"></i>
                         <span style="font-size: 0.6em; font-weight: 900;">${esDigital ? 'DIGITAL' : 'FÍSICO'}</span>
                     </div>
